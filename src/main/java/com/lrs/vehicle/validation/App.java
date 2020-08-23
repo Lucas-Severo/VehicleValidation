1aae3a62604985f7194aa39e4682caae3c25f335
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2020-08-22 21:32:13 -0400

    [REFACTOR] Removido pacotes extras

--- src/main/java/com/lrs/vehicle/validation/App.java
+++
@@ -1,9 +0,0 @@
-package com.lrs.vehicle.validation;
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Validator validator = new RenavamValidator();
-    }
-}
--- src/main/java/com/lrs/vehicle/validation/Validator.java
+++
@@ -1,9 +0,0 @@
-package com.lrs.vehicle.validation;
-
-/**
- *
- * @author lucas
- */
-public interface Validator<T> {
-    boolean validate(T object);
-}
